5df8ac67051a7e4368a71b3d47f57b56c7a4aff0
Hyungjun Kim <hk@plumfare.com>
2013-02-25 12:36:33 -0800

    Model testing

--- src/phylo4j/core/views.py
+++ src/phylo4j/core/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
+from phylo4j.core.models import Enzyme
 
 
 def home(request):
-
-	return HttpResponse('Hello World')
+	enzyme = Enzyme.index.get(enzyme_id_index = '1.1.1.7')
+	return HttpResponse(enzyme.official_name)
