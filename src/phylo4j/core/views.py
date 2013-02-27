bd3055c9132da7bbde7cca8d694550e93a175af7
Hyungjun Kim <hk@plumfare.com>
2013-02-26 23:26:46 -0800

    Added random enzyme generator for presentation

--- src/phylo4j/core/views.py
+++ src/phylo4j/core/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpRequest, HttpResponse
+import random
+
 from phylo4j.util.request import render_page
 from phylo4j.core.models import Enzyme
 
 
 def index(request):
-	enzyme = Enzyme.index.get(enzyme_id_index = '1.1.1.7')
-	return render_page(request, 'index', { 'name' : enzyme.official_name })
+	enzyme_index = '1.1.1.' + str(random.randint(0, 300))
+	enzyme = Enzyme.index.get(enzyme_id_index = enzyme_index)
+	return render_page(request, 'index', { 'name' : enzyme.official_name,
+										   'catalytic_activity' : enzyme.catalytic_activity,
+										   'comment' : enzyme.comments})
